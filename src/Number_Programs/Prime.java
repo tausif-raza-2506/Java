16b772b7ae1a85190db6e649f9a8479cf855df3e
Tausif <tausifraza2506@gmail.com>
2026-08-05 02:14:45 +0530

    Added prime number program

---
+++ src/Number_Programs/Prime.java
@@ -0,0 +1,16 @@
+package Number_Programs;
+import java.util.*;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num, i, c = 0;
+        System.out.println("Enter a number");
+        num = in.nextInt();
+        for (i = 2; i < num; i++) {
+            if(num % i == 0)
+                c++;
+        }
+        System.out.println(c==0?"Prime":"Not a Prime");
+    }
+}
+
