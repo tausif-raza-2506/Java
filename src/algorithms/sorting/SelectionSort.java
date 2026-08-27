af482a6861ae212f46d958a8402769a605a3b455
Tausif <tausifraza2506@gmail.com>
2026-08-28 02:25:30 +0530

    Code for Selection Sort

--- src/algorithms/sorting/SelectionSort.java
+++ src/algorithms/sorting/SelectionSort.java
@@ -1,7 +1,30 @@
 package algorithms.sorting;
+import algorithms.ArrayUtils;
+import java.util.*;
 
 public class SelectionSort {
-    public static void main(String[] args) {
 
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            if (minIndex != i) {
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int arr[] = ArrayUtils.readArray(in);
+        System.out.println("Before sort: " + Arrays.toString(arr));
+        sort(arr);
+        System.out.println("After sort: " + Arrays.toString(arr));
     }
 }
