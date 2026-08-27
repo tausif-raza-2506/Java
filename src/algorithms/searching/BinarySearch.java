40bbc82bf6ed6acc1689a13c6174a876d6d54dd9
Tausif <tausifraza2506@gmail.com>
2026-08-28 00:01:03 +0530

    Code for Linear and Binary Search

--- src/algorithms/searching/BinarySearch.java
+++ src/algorithms/searching/BinarySearch.java
@@ -1,7 +1,30 @@
 package algorithms.searching;
+import algorithms.ArrayUtils;
+import java.util.*;
 
 public class BinarySearch {
-    public static void main(String[] args) {
 
+    public static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == target)
+                return mid;
+            if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int arr[] = ArrayUtils.readArray(in), target = ArrayUtils.readTarget(in);
+        Arrays.sort(arr);
+        int result = search(arr, target);
+
+        System.out.println(result != -1 ? "Element found at index " + result : "Element not found");
     }
 }
--- src/algorithms/searching/LinearSearch.java
+++ src/algorithms/searching/LinearSearch.java
@@ -1,7 +1,23 @@
 package algorithms.searching;
+import algorithms.ArrayUtils;
+import java.util.*;
 
 public class LinearSearch {
-    public static void main(String[] args) {
 
+    public static int search(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[] = ArrayUtils.readArray(in),
+                target = ArrayUtils.readTarget(in),
+                result = search(arr, target);
+
+        System.out.println(result != -1 ? "Element found at index " + result : "Element not found");
     }
 }
