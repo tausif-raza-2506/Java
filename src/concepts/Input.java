993014e1982eedc9e72e7319008a046295fb8450
Tausif <tausifraza2506@gmail.com>
2026-09-10 19:21:55 +0530

    Taking input using scanner and buffered reader class

--- src/concepts/Input.java
+++ src/concepts/Input.java
@@ -1,7 +1,27 @@
 package concepts;
+import java.io.*;
+import java.util.Scanner;
 
 public class Input {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        // Using Scanner Class
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        sc.close();
+
+        // Using BufferedReader Class
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+        try (br) {
+            System.out.println("Enter a number");
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            br.close();
+        }
     }
 }
